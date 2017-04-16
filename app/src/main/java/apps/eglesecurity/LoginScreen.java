e05d8a7b257fb5defe620c2f3d68ae281d8e6b69
Ashraf Iqubal <ashrafiqubal@gmail.com>
2017-04-16 19:11:20 +0530

    First Time
    First Time

--- app/src/main/java/apps/eglesecurity/LoginScreen.java
+++ app/src/main/java/apps/eglesecurity/LoginScreen.java
@@ -2,6 +2,8 @@ package apps.eglesecurity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by ashrafiqubal on 19/03/17.
@@ -13,5 +15,8 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+    }
+    public void logIn(View view){
+        Toast.makeText(getApplicationContext(),"Not Implemented yet",Toast.LENGTH_SHORT).show();
     }
 }
